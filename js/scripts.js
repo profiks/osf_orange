84cda79355f419aec1e169f2d716143e244c5735
profiks <profiks.1994@mail.ru>
2015-01-28 10:19:12 +0200

    slider

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+          $('#banner-fade').bjqs({
+            height      : 450,
+            width       : 700,
+            responsive  : true,
+            centercontrols : false,
+            nexttext : '<div class="nextSlide"></div>', 
+            prevtext : '<div class="prevSlide"></div>',
+            showmarkers : false
+          });
+
+});
