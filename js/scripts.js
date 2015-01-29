4b7ca4797ee25063ebf132c3757478b5926c428a
profiks <profiks.1994@mail.ru>
2015-01-29 16:11:02 +0200

    full resolution done

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-jQuery(document).ready(function($) {
-
-          $('#banner-fade').bjqs({
-            height      : 450,
-            width       : 700,
-            responsive  : true,
-            centercontrols : false,
-            nexttext : '<div class="nextSlide"></div>', 
-            prevtext : '<div class="prevSlide"></div>',
-            showmarkers : false
-          });
-
-});
+$(document).ready(function() {
+    
+    //fancy box video lightbox
+    $('.iframe').fancybox({
+    padding: 0,
+    type: 'iframe',
+    width: 640,
+    height: 360,
+    aspectRatio: true,
+    scrolling: 'no'
+    });
+     
+    
+   // $(".iframe").trigger('click');
+    
+    //fit text for responsivity
+    $(".logo").fitText(0.8,{maxFontSize: '120px' });
+    
+}); //end Dom ready
